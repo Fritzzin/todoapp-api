592b06b9e6587a355b1a9c81c6e631142ca49725
Augusto Fritz <augustofritzz@gmail.com>
2025-12-11 10:31:40 -0300

    Adding swaggerUI descriptions

--- TodoApp.Api/Endpoints/TodosEndpoints.cs
+++ TodoApp.Api/Endpoints/TodosEndpoints.cs
@@ -9,33 +9,39 @@ public static class TodosEndpoints
     public static void AddEndpointsTodos(this WebApplication app)
     {
         app.MapPost("/todos", async (CreateTodoRequestDto request, ITodoService todoService) =>
-        {
-            var newTodoId = await todoService.CreateTodoAsync(request.Title, request.Description);
+            {
+                var newTodoId = await todoService.CreateTodoAsync(request.Title, request.Description);
 
-            return Results.Created($"/todos/{newTodoId}", newTodoId);
-        }).AddEndpointFilter<ValidationFilter<CreateTodoRequestDto>>();
+                return Results.Created($"/todos/{newTodoId}", newTodoId);
+            })
+            .AddEndpointFilter<ValidationFilter<CreateTodoRequestDto>>()
+            .WithDescription("Creates a Todo");
 
-        app.MapGet("/todos", async (ITodoService todoService) => await todoService.GetAllAsync());
+        app.MapGet("/todos", async (ITodoService todoService) => await todoService.GetAllAsync())
+            .WithDescription("Gets all todos");
 
         app.MapGet("/todos/{id}", async (Guid id, ITodoService todoService) =>
-        {
-            var result = await todoService.GetByIdAsync(id);
+            {
+                var result = await todoService.GetByIdAsync(id);
 
-            return result == null ? Results.NotFound() : Results.Json(result);
-        });
+                return result == null ? Results.NotFound() : Results.Json(result);
+            })
+            .WithDescription("Get one of the todos based on ID");
 
         app.MapPut("/todos/{id}", async (Guid id, UpdateTodoRequestDto request, ITodoService todoService) =>
-        {
-            var isUpdated = await todoService.UpdateTodoAsync(id, request.title, request.description);
+            {
+                var isUpdated = await todoService.UpdateTodoAsync(id, request.title, request.description);
 
-            return isUpdated ? Results.NoContent() : Results.NotFound();
-        });
+                return isUpdated ? Results.NoContent() : Results.NotFound();
+            })
+            .WithDescription("Updates one of the todos based on ID");
 
         app.MapDelete("/todos/{id}", async (Guid id, ITodoService todoService) =>
-        {
-            var isDeleted = await todoService.DeleteTodoAsync(id);
+            {
+                var isDeleted = await todoService.DeleteTodoAsync(id);
 
-            return isDeleted ? Results.NoContent() : Results.NotFound();
-        });
+                return isDeleted ? Results.NoContent() : Results.NotFound();
+            })
+            .WithDescription("Deletes one of the todos based on ID");
     }
 }
